917beda02c5aa605bd41c734364627c89944de0e
kamalafzal2016 <58792992+kamalafzal2016@users.noreply.github.com>
2019-12-12 04:18:11 +0500

    Add files via upload

---
+++ mysite/urls.py
@@ -0,0 +1,33 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from django.views import View
+
+class Index(View):
+    def get(self,request):
+        return HttpResponse("Hello, world. You're at the polls index.")
+        
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Python anywhere Platform.")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',Index.as_view(), name="main page")
+]
